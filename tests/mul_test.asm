8f4337d39c7c3df9f69fa76d864e96502025454a
Michael Vaden <michaelvaden.mjv@gmail.com>
2025-11-18 18:13:27 -0500

    added test

---
+++ tests/mul_test.asm
@@ -0,0 +1,30 @@
+ORG &H0
+START:  
+	LOADI 3		; ACC <- 3
+    OUT &H92	; A = 3
+    
+    LOADI 5		; ACC <- 3
+    OUT &H93	; B = 3
+    
+    LOADI 5		; ACC <- 1
+    OUT &H90	; CTRL/STATUS write: set START
+    
+WAIT:   ; --- Poll until BUSY clears (or DONE sets) ---
+	IN &H90     ; ACC <- STATUS
+    AND &H0001  ; mask START/BUSY/DONE as needed
+    JNZ WAIT    ; keep looping while bit is non-zero
+                           
+    ; --- Read result LO/HI ---
+    IN      &H94        ; ACC <- LO word (low 16 bits of product)
+    STORE   RESULT_LO   ; store to memory
+
+    IN      &H95        ; ACC <- HI word (high 16 bits of product)
+    STORE   RESULT_HI
+
+DONE:
+	JUMP DONE
+
+; Data section
+ORG &H100
+	RESULT_LO:  &H0000   ; expected 0x000F (15)
+	RESULT_HI: 	&H0000   ; expected 0x0000
